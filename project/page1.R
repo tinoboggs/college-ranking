a6c1c40775689c19960d479ded3ca90db5f3200e
Ryan Yee <ryee2@wisc.edu>
2022-11-18 13:11:45 -0600

    added prototype of brushed histogram

--- project/page1.R
+++ project/page1.R
@@ -21,7 +21,8 @@ filter_data <- function(act_score, selectivity, min_admit, home_state, budget, s
       inst_type = case_when(
         TYPE == "PRIV" ~ "Private",
         TYPE == "PUB" ~ "Public"
-      )
+      ),
+      avg_yearly_cost = COSTT4_A / 4
     ) %>%
     filter(
       YEAR == 2020,
@@ -43,6 +44,7 @@ ui <- fluidPage(
          checkboxGroupInput("selectivity", "Selectivity", c("Reach", "Target", "Safety"), selected = c("Reach", "Target", "Safety")),
          sliderInput("min_admit", "Minimum Admit Rate", 0, 1, 0),
          selectInput("home_state", "Home State", state.abb, selected = "WI"),
+         plotOutput("cost", brush = brushOpts("cost_brush", direction = "x"), height = 100),
          sliderInput("budget", "Tuition Range (per year)", 0, 70000, c(0, 70000), pre = "$"),
          sliderInput("size", "Student Body Size", 0, 80000, c(0, 80000)),
          checkboxGroupInput("type", "Institution Type", c("Public", "Private"), selected = c("Public", "Private"))
@@ -59,6 +61,17 @@ ui <- fluidPage(
 server <- function(input, output) {
   app_data <- reactive({
     filter_data(input$act_score, input$selectivity, input$min_admit, input$home_state, input$budget, input$size, input$type)
+  })
+  output$cost <- renderPlot({
+    app_data() %>%
+      ggplot() +
+      geom_histogram(aes(avg_yearly_cost)) +
+      labs(
+        title = "Yearly Cost of Attendance"
+      ) +
+      theme(
+        axis.title = element_blank()
+      )
   })
   output$map <- renderLeaflet({
     app_data() %>%
